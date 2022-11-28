eb53547175a0382b4522430b489b239f6c6a9638
Aleksandr Vasiljev <zaswet@gmail.com>
2022-11-28 11:42:08 +0200

    category create/ update moved to model

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,17 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Category;
-use App\Http\Requests\StoreCategoryRequest;
-use App\Http\Requests\UpdateCategoryRequest;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    
     public function list()
     {
         return view('category.list', [
@@ -21,52 +15,20 @@ class CategoryController extends Controller
         ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('category.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \App\Http\Requests\StoreCategoryRequest  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
-        $request->validate([
-            'category' => 'required'
-        ]);
-
-        Category::create([
-            'category' => $request->category
-        ]);
+        $cat = new Category();
+        $cat->categoryStore($request);      
 
         return redirect()->route('category.list');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Category $category)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
+    
     public function edit(Category $category)
     {
 
@@ -75,32 +37,14 @@ class CategoryController extends Controller
         ]);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \App\Http\Requests\UpdateCategoryRequest  $request
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, Category $category)
     {
-        $request->validate([
-            'category' => 'required'
-        ]);
-
-        $category->update([
-            'category' => $request->category
-        ]);
+        $cat = new Category();
+        $cat->categoryUpdate($request, $category);
 
         return redirect()->route('category.list');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Category  $category
-     * @return \Illuminate\Http\Response
-     */
     public function destroy(Category $category)
     {
         $category->getBook()->delete();
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -15,4 +15,28 @@ class Category extends Model
     {
         return $this->hasMany(Book::class, 'category_id', 'id');
     }
+
+    public function categoryStore(object $request) : void
+    {
+        $request->validate([
+            'category' => 'required'
+        ]);
+        
+        $this::create([
+            'category' => $request->category
+        ]);
+
+        
+    }
+
+    public function categoryUpdate(object $request, object $category) : void
+    {
+        $request->validate([
+            'category' => 'required'
+        ]);
+
+        $category->update([
+            'category' => $request->category
+        ]);
+    }
 }
